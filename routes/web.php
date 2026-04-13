8dcd3de363e707cfbad79104d3945488d2a3aa16
Brian Henry <brianh@idealogical.com>
2026-04-13 13:16:24 -0400

    components, props, and attributs

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// static pages
+Route::view('/', 'welcome');
+Route::view('/about', 'about');
 
-Route::get('/about', function () {
-    return view('about');
-});
-
+// complex routes
 Route::get('/contact', function () {
     return view('contact');
 });
