d8766fbf18c6b8500c6fc11b07559544362ca87d
Brian Henry <brianh@idealogical.com>
2026-04-20 13:43:33 -0400

    moving logic into controllers

---
+++ resources/views/ideas/create.blade.php
@@ -0,0 +1,25 @@
+<x-layout title="Create Idea">
+
+    <h1>Create Idea</h1>
+
+    <form method="POST" action="/ideas">
+        @csrf
+        <div class="w-full max-w-sm">
+            <label for="description" class="block mb-2 text-sm font-medium text-gray-200">New Idea</label>
+            <textarea 
+                id="description" 
+                name="description"
+                rows="4" 
+                class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500" 
+                placeholder="Write your idea here..."></textarea>
+        </div>
+
+        <div class="mt-6 flex items-center gap-x-6">
+            <button type="submit" class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+                Save
+            </button>
+        </div>
+
+    </form>
+
+</x-layout>
--- resources/views/ideas/index.blade.php
+++ resources/views/ideas/index.blade.php
@@ -2,27 +2,8 @@
 
     <h1>Ideas</h1>
 
-    <form method="POST" action="/ideas">
-        @csrf
-        <div class="w-full max-w-sm">
-            <label for="description" class="block mb-2 text-sm font-medium text-gray-200">New Ideas</label>
-            <textarea 
-                id="description" 
-                name="description"
-                rows="4" 
-                class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500" 
-                placeholder="Write your idea here..."></textarea>
-        </div>
-
-        <div class="mt-6 flex items-center gap-x-6">
-            <button type="submit" class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-                Save
-            </button>
-        </div>
-
-    </form>
-
     @if ($ideas->count())
+
         <div>
             <h2>Saved Ideas</h2>
 
@@ -46,6 +27,11 @@
             </div>
 
         </form>
+
+    @else
+
+        <p class="text-sm text-gray-500">No ideas yet. <a href="/ideas/create" class=class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">Create one</a>.</p>
+
     @endif
 
 </x-layout>
